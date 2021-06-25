e219a20d539bdf1316312bfbdd9d608b6f0f9cb7
Anderson Cardoso <andercosta87@gmail.com>
2021-06-25 00:13:46 -0300

    Aula -5

---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,18 @@
+
+
+import {Request, Response, NextFunction} from "express";
+
+export function ensureAuthenticated(
+  request:Request,
+  response:Response,
+  next:NextFunction
+  ) {
+    //Receber o token
+    
+    //validar se o token está preenchido
+
+    //validar se o token é valido
+
+    //Recuperar informações do usuário
+    
+}
